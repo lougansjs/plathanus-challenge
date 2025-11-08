f70785086d4932eedccc804ad932784e541d2737
lougansjs <lougans.lougans99@gmail.com>
2025-11-08 16:31:11 -0300

    component: add PropertyGallery component

---
+++ app/frontend-plathanus-real-state/app/components/properties/PropertyGallery.vue
@@ -0,0 +1,27 @@
+<!-- app/components/properties/PropertyGallery.vue -->
+<script setup lang="ts">
+import type { PhotoUrls } from '../../../types/property'
+
+// @ts-ignore - defineProps and defineEmits are auto-imported by Nuxt
+const props = defineProps<{ photos: PhotoUrls[] }>()
+const emit = defineEmits<{
+  'photo-click': [index: number]
+}>()
+
+function handlePhotoClick(index: number) {
+  emit('photo-click', index)
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
+    <img
+      v-for="(ph, i) in photos"
+      :key="i"
+      :src="ph.thumb_url"
+      @click="handlePhotoClick(i)"
+      class="w-full h-36 object-cover rounded-lg cursor-pointer hover:opacity-80 transition-opacity"
+      alt="photo"
+    />
+  </div>
+</template>
